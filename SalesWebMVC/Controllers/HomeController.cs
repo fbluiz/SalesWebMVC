3ec77382e6a0524e8f0c1d95a7367ba17eb78d49
Fabio Luiz <fbluiz.sf@gmail.com>
2022-09-25 10:16:27 -0300

    Primeiro controlador e páginas Razor

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App by Fábio Luiz";
+            ViewData["Email"] = "fbluiz.sf@gmail.com";
 
             return View();
         }
