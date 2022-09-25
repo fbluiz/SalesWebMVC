67dc7c477069e74a5a867770dcabe8395e02f729
Fabio Luiz <fbluiz.sf@gmail.com>
2022-09-25 11:13:19 -0300

    Primeiro Model-Controller-View - Department

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
--- SalesWebMVC/obj/Debug/netcoreapp2.1/Razor/Views/Shared/Error.cshtml.g.cs
+++ SalesWebMVC/obj/Debug/netcoreapp2.1/Razor/Views/Shared/Error.cshtml.g.cs
@@ -19,7 +19,7 @@ using SalesWebMVC;
 #line default
 #line hidden
 #line 2 "C:\App Web\SalesWebMVC\SalesWebMVC\Views\_ViewImports.cshtml"
-using SalesWebMVC.Models;
+ using SalesWebMVC.Models.ViewModels;
 
 #line default
 #line hidden
