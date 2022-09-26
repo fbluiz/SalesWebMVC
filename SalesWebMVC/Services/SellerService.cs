6147775fffad18b1ae864a547a4f2a5725d97f95
Fabio Luiz <fbluiz.sf@gmail.com>
2022-09-26 15:37:29 -0300

    Tela de detalhes

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace SalesWebMVC.Services
 {
@@ -27,9 +29,8 @@ namespace SalesWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
-
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
