b42d610912eb56eb5ef7448a2df42f44bbdd762f
Fabio Luiz <fbluiz.sf@gmail.com>
2022-09-26 14:55:38 -0300

    Delete seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -14,16 +14,26 @@ namespace SalesWebMVC.Services
         {
             _context = context;
         }
-
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
 
         }
-
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
